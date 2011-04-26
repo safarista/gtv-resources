a9da850e4a9d3c6c73163979887737ca1b258cb2
wiktorgworek@google.com <wiktorgworek@google.com@eebe47aa-606d-aedd-3425-2dbd6e8cf17f>
2011-04-26 09:44:02 +0000

    Maintenance update.

--- trunk/gtv-closure-demo/static/tv-ui/deps.js
+++ trunk/gtv-closure-demo/static/tv-ui/deps.js
@@ -2,7 +2,7 @@
 // Please do not edit.
 goog.addDependency('../../../tv-ui/button.js', ['tv.ui.Button'], ['tv.ui', 'tv.ui.Component']);
 goog.addDependency('../../../tv-ui/component.js', ['tv.ui.Component'], ['goog.events', 'goog.events.EventHandler', 'goog.events.EventTarget', 'goog.style', 'tv.ui', 'tv.ui.Document']);
-goog.addDependency('../../../tv-ui/container.js', ['tv.ui.Container'], ['goog.dom', 'goog.dom.classes', 'goog.math.Coordinate', 'goog.style', 'tv.ui', 'tv.ui.Component']);
+goog.addDependency('../../../tv-ui/container.js', ['tv.ui.Container'], ['goog.Timer', 'goog.dom', 'goog.dom.classes', 'goog.math.Coordinate', 'goog.style', 'goog.userAgent.product', 'tv.ui', 'tv.ui.Component']);
 goog.addDependency('../../../tv-ui/decorate_handler.js', ['tv.ui.DecorateHandler'], ['goog.array']);
 goog.addDependency('../../../tv-ui/deps.js', [], []);
 goog.addDependency('../../../tv-ui/document.js', ['tv.ui.Document'], ['goog.events.EventHandler', 'goog.events.EventType', 'goog.events.KeyCodes', 'goog.events.KeyHandler']);
@@ -10,7 +10,9 @@ goog.addDependency('../../../tv-ui/grid.js', ['tv.ui.Grid'], ['tv.ui.Container']
 goog.addDependency('../../../tv-ui/input.js', ['tv.ui.Input'], ['goog.dom', 'goog.dom.selection', 'goog.events.InputHandler', 'tv.ui', 'tv.ui.Component']);
 goog.addDependency('../../../tv-ui/lightbox.js', ['tv.ui.Lightbox'], ['tv.ui.Container']);
 goog.addDependency('../../../tv-ui/link.js', ['tv.ui.Link'], ['tv.ui', 'tv.ui.Button']);
-goog.addDependency('../../../tv-ui/menu.js', ['tv.ui.Menu'], ['tv.ui.Button', 'tv.ui.TabContainer']);
+goog.addDependency('../../../tv-ui/menu.js', ['tv.ui.Menu'], ['goog.functions', 'tv.ui.Container']);
+goog.addDependency('../../../tv-ui/scroll_pane.js', ['tv.ui.ScrollPane'], ['goog.asserts', 'goog.dom.classes', 'goog.style', 'tv.ui', 'tv.ui.Button', 'tv.ui.Container']);
+goog.addDependency('../../../tv-ui/sub_menu.js', ['tv.ui.SubMenu'], ['goog.functions', 'tv.ui.Container']);
 goog.addDependency('../../../tv-ui/tab_container.js', ['tv.ui.TabContainer'], ['tv.ui.Container']);
 goog.addDependency('../../../tv-ui/toggle_button.js', ['tv.ui.ToggleButton'], ['tv.ui', 'tv.ui.Button']);
 goog.addDependency('../../../tv-ui/ui.js', ['tv.ui'], ['goog.array', 'goog.dom']);
